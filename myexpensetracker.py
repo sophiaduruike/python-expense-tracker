7ca2d7fceff4a685e537b5d3dec943c1a3a54c41
Sophia Duruike <sophiaduruike@gmail.com>
2026-07-23 13:16:48 +0100

    V2: add categories, dictionary-based totals, refactor into functions

--- myexpensetracker.py
+++ myexpensetracker.py
@@ -1,21 +1,36 @@
 commodity_list = [ ]
 commodity_prices = [ ]
+category_list = [ ]
 total_amount_spent = 0
-commodity_name = input("Enter name of commodity bought, and enter 'done' to signify the end of commodities: ")
+category_totals = { }
 
-while commodity_name != "done":
-    commodity_amount = int(input("Enter price of commodity: "))
+def add_expense():
+    global commodity_name
+    global total_amount_spent
+
     commodity_list.append(commodity_name)
+
+    commodity_amount = int(input("Enter price of commodity in Naira: "))
     commodity_prices.append(commodity_amount)
     total_amount_spent += commodity_amount
+
+    commodity_category = input("Enter category of commodity. E.g 'Food', 'Transport', 'Data' ")
+    category_list.append(commodity_category)
+
+    category_totals[commodity_category ] = category_totals.get(commodity_category, 0) + commodity_amount
     commodity_name = input("Enter name of commodity bought, and enter 'done' to signify the end of commodities: ")
 
-for index in range(len(commodity_list)):
-    print(f"you have spent {commodity_prices[index]} on {commodity_list[index]}")
+def print_report():
+    average_amount_per_expense = total_amount_spent / len(commodity_list)
+    print("====EXPENSE REPORT====")
+    print(f"List of items bought: {commodity_list}")
+    print(f"List of  amounts spent: {commodity_prices}")
+    print(f"Total amount spent: {total_amount_spent}")
+    print(f"The average amount per expense is: {average_amount_per_expense}")
+    print(f"Money spent on eachcategory {category_totals}")
 
-print("====EXPENSE REPORT====")
-print(f"List of itrms bought: {commodity_list}")
-print(f"List of  amounts spent: {commodity_prices}")
-print(f"Total amount spent: {total_amount_spent}")
+commodity_name = input("Enter name of commodity bought, and enter 'done' to signify the end of commodities: ")
+while commodity_name != "done":
+    add_expense()
 
-
+print_report()
